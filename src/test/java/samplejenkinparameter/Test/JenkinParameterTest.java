8c95ab8acc57bad1c1697208dd241b6fdee5bf8c
server <server@user>
2022-05-31 11:52:15 +0530

    jenkinparameter

---
+++ src/test/java/samplejenkinparameter/Test/JenkinParameterTest.java
@@ -0,0 +1,28 @@
+package samplejenkinparameter.Test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class JenkinParameterTest {
+	@Test
+	public void parameter()
+	{
+		WebDriver driver=null;
+		String browser=System.getProperty("browser");
+		
+		String url=System.getProperty("url");
+		switch(browser)
+		{
+		case "chrome":
+			driver=new ChromeDriver();
+			break;
+		case "firefox":
+			driver=new FirefoxDriver();
+			break;
+		}
+		driver.get(url);
+		driver.quit();
+	}
+}
